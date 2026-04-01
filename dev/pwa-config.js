e7806c7bc6b739cdbbc307fdaac9809896c71254
apdaga <39180163+apdaga@users.noreply.github.com>
2026-04-01 17:47:19 +0530

    Update CACHE_NAME to version v2

--- dev/pwa-config.js
+++ dev/pwa-config.js
@@ -9,5 +9,5 @@ const CLINICSYNC_CONFIG = {
   SCRIPT_URL:  "https://script.google.com/macros/s/AKfycbzV8pUvj4GMaXlnMhh7MkgfDleN8X1frH9Q-L0g4GHPlQacOyY3ozz2SPAuyccAmnJV/exec",
   ENV:         "DEV",
   CLIENT_NAME: "ClinicSync DEV",
-  CACHE_NAME:  "csync-dev-v1"
+  CACHE_NAME:  "csync-dev-v2"
 };
